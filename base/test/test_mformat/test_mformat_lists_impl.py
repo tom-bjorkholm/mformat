5940274be43eb8ef97dd4531379d67dbf42576d4
Tom Björkholm <klausuler_linnet0q@icloud.com>
2026-01-11 16:20:53 +0100

    WIP: test abstract methods

--- base/test/test_mformat/test_mformat_lists_impl.py
+++ base/test/test_mformat/test_mformat_lists_impl.py
@@ -7,7 +7,7 @@
 
 import pytest
 from check_capsys import check_capsys
-from mformat.mformat_state import MultiFormatState
+from mformat.mformat_state import MultiFormatState, Formatting
 from mformat.mformat_lists_impl import ListHandlerMixin
 
 
@@ -28,4 +28,27 @@ def test_is_in_item_state(capsys, state: MultiFormatState,
     list_handler.state = state
     # pylint: disable=protected-access
     assert list_handler._is_in_list_item_state() == in_item
+    check_capsys(capsys)
+
+
+@pytest.mark.parametrize('method,args',
+                         [('_end_state', ()),
+                          ('_to_write', ('test', True, False)),
+                          ('_write_text', ('test', MultiFormatState.PARAGRAPH,
+                                           Formatting(bold=False,
+                                                      italic=False))),
+                          ('_start_bullet_list', [1]),
+                          ('_start_numbered_list', [1]),
+                          ('_end_bullet_list', [1]),
+                          ('_end_numbered_list', [1]),
+                          ('_start_bullet_item', [1]),
+                          ('_start_numbered_item', [1, 1, '1.']),
+                          ('_end_bullet_item', [1]),
+                          ('_end_numbered_item', [1, 1])])
+def test_abstract_methods(capsys, method: str, args: tuple[int, ...]) -> None:
+    """Test the abstract methods."""
+    list_handler = ListHandlerMixin()
+    with pytest.raises(NotImplementedError) as exc:
+        getattr(list_handler, method)(*args)
+    assert exc.value.args[0] == f'{method} must be overridden'
     check_capsys(capsys)
