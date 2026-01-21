66a91086ea247f7887b94705b1018112b153f6ff
Tom Björkholm <klausuler_linnet0q@icloud.com>
2026-01-21 12:56:30 +0100

    example of writing a table row by row

---
+++ example/src/e16_table_row_by_row.py
@@ -0,0 +1,40 @@
+#! /usr/local/bin/python3
+"""Example of writing a table row by row."""
+
+# Copyright (c) 2025 - 2026 Tom Björkholm
+# MIT License
+#
+from e01_paragraph import example_main
+from mformat.factory import create_mf
+
+
+NOTE = '''
+Note: As the rows are added and written one by one, the library
+cannot know the width of columns in future rows, this will
+make markdown formatted output look a bit odd when reading
+the markdown file, however any further formatting from markdown
+will hide this and make the table look as expected.
+'''.replace('\n', ' ')
+
+
+def example_table_row_by_row(format_name: str, file_name: str) -> None:
+    """Show how to write a table row by row."""
+    # We start by getting the MultiFormat class from the factory
+    # as a context manager as we did in exampe e01_paragraph.py.
+    with create_mf(format_name=format_name, file_name=file_name) as mf:
+        # Start a table.
+        mf.start_table(first_row=['Name', 'Age', 'City'])
+        # Add a row.
+        mf.add_table_row(row=['John', '25', 'New York'])
+        # Add a row.
+        mf.add_table_row(row=['Jane', '30', 'Los Angeles'])
+        # Add a row.
+        mf.add_table_row(row=['Jim', '35', 'Chicago'])
+        # Table is automatically ended when the context manager is exited,
+        # or when we start something else.
+        mf.start_paragraph(text=NOTE)
+
+
+if __name__ == '__main__':
+    example_main(example_text='Writing a table row by row',
+                 function=example_table_row_by_row)
---
+++ example/src/e17_table_row_by_row_bold.py
@@ -0,0 +1,39 @@
+#! /usr/local/bin/python3
+"""Example of writing a table row by row with bold and italic text."""
+
+# Copyright (c) 2025 - 2026 Tom Björkholm
+# MIT License
+#
+from e01_paragraph import example_main
+from e16_table_row_by_row import NOTE
+from mformat.factory import create_mf
+
+
+def example_table_row_by_row_bold(format_name: str, file_name: str) -> None:
+    """Show how to write a table row by row with bold and italic text."""
+    # We start by getting the MultiFormat class from the factory
+    # as a context manager as we did in exampe e01_paragraph.py.
+    with create_mf(format_name=format_name, file_name=file_name) as mf:
+        # Start first row of table with bold first line.
+        mf.start_table(first_row=['City', 'Country', 'Size'], bold=True)
+        # Add a row.
+        mf.add_table_row(row=['Mariehamn', 'Finland', 'Small'])
+        mf.add_table_row(row=['Copenhagen', 'Denmark', 'Large'], italic=True)
+        mf.add_table_row(row=['Tokyo', 'Japan', 'Huge'], bold=True,
+                         italic=True)
+        # Table is automatically ended when the context manager is exited,
+        # or when we start something else, like another table.
+        mf.start_table(first_row=['Capital', 'Country', 'Continent'],
+                       italic=True)
+        mf.add_table_row(row=['Oslo', 'Norway', 'Europe'])
+        mf.add_table_row(row=['Tokyo', 'Japan', 'Asia'])
+        mf.add_table_row(row=['Berlin', 'Germany', 'Europe'], bold=True)
+        mf.add_table_row(row=['Kairo', 'Egypt', 'Africa'])
+        mf.add_table_row(row=['Brussels', 'Belgium', 'Europe'], bold=True,
+                         italic=True)
+        mf.start_paragraph(text=NOTE)
+
+
+if __name__ == '__main__':
+    example_main(example_text='Writing a table row by row (bold and italic)',
+                 function=example_table_row_by_row_bold)
