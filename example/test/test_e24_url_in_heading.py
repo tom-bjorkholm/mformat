6beb51c37203d3d5c517de467edc8b2765f1cbb4
Tom Björkholm <klausuler_linnet0q@icloud.com>
2026-02-18 15:36:24 +0100

    text e24_url_in_heading example

---
+++ example/test/test_e24_url_in_heading.py
@@ -0,0 +1,152 @@
+#! /usr/local/bin/python3
+"""Test the e23_url_in_numbered_list example."""
+
+# Copyright (c) 2026 Tom Björkholm
+# MIT License
+#
+
+import sys
+from pathlib import Path
+import pytest  # pylint: disable=unused-import
+from test_e01_paragraph import EXPECTED_HTML_PRE, EXPECTED_HTML_POST, EXPECTED_ODT_PRE
+from example_checkers import (
+    check_markdown_func, check_capsys_silent, check_html_func,
+    check_docx_func, check_odt_func, docx_version_of_html, odt_version_of_html)
+# Add example/src to path
+# pylint: disable=duplicate-code
+_example_test_path = (
+    Path(__file__).parent.parent / 'src'
+)
+sys.path.insert(0, str(_example_test_path))
+from e24_url_in_heading import example_url_in_heading  # pylint: disable=wrong-import-position,import-error # noqa: E402,E501
+
+
+
+EXPECTED_MD_TEXT = [
+    '# URL in heading example\n\n',
+    '# A heading with a URL: '
+    '[This URL link to the examples.]'
+    '(https://bitbucket.org/tom-bjorkholm/mformat/src/master/example)\n\n',
+    '## A heading with a bold URL: '
+    '**[This bold URL link to the example source code.]'
+    '(https://bitbucket.org/tom-bjorkholm/mformat/src/master/example/src)**\n\n',
+    '## A heading with an italic URL: '
+    '*[This italic URL link to the examples result.]'
+    '(https://bitbucket.org/tom-bjorkholm/mformat/src/master/example/result)*\n\n',
+    '## And with an italic and bold URL: '
+    '***[This italic and bold URL link to the examples.]'
+    '(https://bitbucket.org/tom-bjorkholm/mformat/src/master/example/src)***\n\n',
+    'The add_url function can be used to add a URL to a heading, as well as to',
+    'paragraphs, bullet lists, and numbered point lists.\n'
+]
+EXPECTED_HTML_BODY_TEXT = [
+    '<h1>',
+    'URL in heading example',
+    '</h1>',
+    '<h1>',
+    'A heading with a URL:',
+    '<a href="https://bitbucket.org/tom-bjorkholm/mformat/src/master/'
+    'example">',
+    'This URL link to the examples.',
+    '</a>',
+    '</h1>',
+    '<h2>',
+    'A heading with a bold URL:',
+    '<strong>',
+    '<a href="https://bitbucket.org/tom-bjorkholm/mformat/src/master/'
+    'example/src">',
+    'This bold URL link to the example source code.',
+    '</a>',
+    '</strong>',
+    '</h2>',
+    '<h2>',
+    'A heading with an italic URL:',
+    '<em>',
+    '<a href="https://bitbucket.org/tom-bjorkholm/mformat/src/master/'
+    'example/result">',
+    'This italic URL link to the examples result.',
+    '</a>', '</em>', '</h2>',
+    '<h2>',
+    'And with an italic and bold URL:',
+    '<em>', '<strong>',
+    '<a href="https://bitbucket.org/tom-bjorkholm/mformat/src/master/'
+    'example/src">',
+    'This italic and bold URL link to the examples.', 
+    '</a>', '</strong>', '</em>', '</h2>', '<p>',
+    'The add_url function can be used to add a URL to a heading,',
+    'as well as to',
+    'paragraphs, bullet lists, and numbered point lists.',
+    '</p>'
+]
+EXPECTED_DOCX_HTML_TEXT = [
+    '<h1>',
+    'URL in heading example',
+    '</h1>',
+    '<h1>',
+    'A heading with a URL:',
+    '<a href="https://bitbucket.org/tom-bjorkholm/mformat/src/master/'
+    'example">',
+    'This URL link to the examples.',
+    '</a>',
+    '</h1>',
+    '<h2>',
+    'A heading with a bold URL:',
+    '<a href="https://bitbucket.org/tom-bjorkholm/mformat/src/master/'
+    'example/src">',
+    'This bold URL link to the example source code.',
+    '</a>',
+    '</h2>',
+    '<h2>',
+    'A heading with an italic URL:',
+    '<a href="https://bitbucket.org/tom-bjorkholm/mformat/src/master/'
+    'example/result">',
+    'This italic URL link to the examples result.',
+    '</a>',
+    '</h2>',
+    '<h2>',
+    'And with an italic and bold URL:',
+    '<a href="https://bitbucket.org/tom-bjorkholm/mformat/src/master/'
+    'example/src">',
+    'This italic and bold URL link to the examples.', 
+    '</a>',
+    '</h2>', '<p>',
+    'The add_url function can be used to add a URL to a heading,',
+    'as well as to paragraphs, bullet lists, and numbered point lists.',
+    '</p>'
+]
+EXPECTED_ODT_HTML_TEXT = EXPECTED_DOCX_HTML_TEXT
+EXPECTED_HTML_TEXT = EXPECTED_HTML_PRE + EXPECTED_HTML_BODY_TEXT + EXPECTED_HTML_POST
+EXPECTED_ODT_BODY_TEXT = odt_version_of_html(EXPECTED_ODT_HTML_TEXT)
+EXPECTED_ODT_TEXT = EXPECTED_ODT_PRE + EXPECTED_ODT_BODY_TEXT + EXPECTED_HTML_POST
+
+
+def test_e24_url_in_headings_md(capsys):
+    """Test the example_url_in_heading function with the md format."""
+    expected_txt = EXPECTED_MD_TEXT
+    # MD025: Multiple top level headings in the same document
+    expected_error = ['MD025']
+    check_markdown_func(example_url_in_heading, expected_txt,
+                        expected_error=expected_error)
+    check_capsys_silent(capsys)
+
+
+def test_e24_url_in_headings_html(capsys):
+    """Test the example_url_in_heading function with the html format."""
+    expected_txt = EXPECTED_HTML_TEXT
+    check_html_func(example_url_in_heading, expected_txt)
+    check_capsys_silent(capsys)
+
+
+def test_e24_url_in_headings_docx(capsys):
+    """Test the example_url_in_heading function with the docx format."""
+    expected_txt = docx_version_of_html(EXPECTED_DOCX_HTML_TEXT)
+    expected_warnings = []
+    check_docx_func(example_url_in_heading, expected_txt, expected_warnings)
+    check_capsys_silent(capsys)
+
+
+def test_e24_url_in_headings_odt(capsys):
+    """Test the example_url_in_heading function with the odt format."""
+    expected_txt = EXPECTED_ODT_TEXT
+    check_odt_func(example_url_in_heading, expected_txt)
+    check_capsys_silent(capsys)
